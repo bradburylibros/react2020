4948a465c2b517fd0cf18cccffcc4ce0b2d3a720
Silvia Sosa <silvia_sosa@hotmail.com>
2020-12-25 15:20:38 -0300

    retomando

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function home() {
     return (
-        <div>
-            
+        <div className="Home">
+        <div className="container">
+            <div className="row">
+            <div className="col-6 mt-5">
+                {/* <img src={Logo} alt="logo conferencia" /> */}
+                <div className="text-center mt-3">
+                {/* <Link to="/???" className="btn btn-info btn-lg">
+                    Accede a ...
+                </Link> */}
+                </div>
+            </div>
+            <div className="col-6 mt-5 contenedor">
+                {/* <img className="logo_bradbury" src={Logo} alt="logo Bradbury" /> */}
+            </div>
+            </div>
+        </div>
         </div>
     )
 }
