3260f20918742aaed074bd448a9d96ab017f3645
Franco <francoricci_93@hotmail.com>
2020-12-27 19:24:26 -0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
 //import { Link } from "react-router-dom";
-<<<<<<< HEAD
-import { Container, Row, Col } from "react-bootstrap";
-import CarouselHome from "../componentes/CarouselHome";
-import Suscribirse from "../componentes/Suscribirse";
-import Footer from "../componentes/Footer";
-import NavBar from "../componentes/NavBar";
-=======
 import {Container, Row, Col} from 'react-bootstrap'
 
 import NavBar from '../componentes/NavBar'
@@ -16,7 +9,6 @@ import CarouselHome from "../componentes/CarouselHome";
 import Libro from '../componentes/Libro'
 import Footer from '../componentes/Footer'
 
->>>>>>> 7582c3b2219c550b7f681becb8884bf3236d9b53
 import "../css/style.css";
 // ----------------------------------------------------- //
 
@@ -53,12 +45,9 @@ export default function Home() {
         {/* NAVBAR */}
         <Row>
           <Col>
-<<<<<<< HEAD
-=======
             <NavBar />
                 {/* Barra de busqueda  I*/}
                 <LibroBarraBusq buscarTxt={buscarTxt} handleChange={handleChange} />
->>>>>>> 7582c3b2219c550b7f681becb8884bf3236d9b53
           </Col>
         </Row>
         {/* CAROUSEL */}
@@ -68,9 +57,6 @@ export default function Home() {
         </Row>
         {/* CARDS */}
         <Row>
-<<<<<<< HEAD
-          <Col>{/* AQUI VAN LAS CARDS */}</Col>
-=======
           <Col>
             {/* AQUI VAN LAS CARDS */}
             {/* POR EL MOMENTO PUSE EL LIBROAPP PERO DEBERIA IR EL LIBRODECK
@@ -81,7 +67,6 @@ export default function Home() {
             {/* <LibroApp/> */}
             <Libro/>
           </Col>
->>>>>>> 7582c3b2219c550b7f681becb8884bf3236d9b53
         </Row>
         {/* FOOTER */}
         <Row>
