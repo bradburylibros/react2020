6dcf960240fcc82c2deb6a8070b5b63b29e06f07
Franco <francoricci_93@hotmail.com>
2020-12-31 17:12:59 -0300

    Formato codigo

--- src/componentes/LibroCard.jsx
+++ src/componentes/LibroCard.jsx
@@ -1,49 +1,49 @@
 import React from "react";
-import { Card, Badge, Button } from "react-bootstrap";
+import { Badge, Button } from "react-bootstrap";
 import LibroTapa from "./LibroTapa";
 import "../css/style.css";
 
-
 export default function LibroCard({ libro }) {
   return (
-    <section  className="d-flex" id="cardslibros">
-		<div className=" mb-0">
-      <div className="card h-100  mt-5">
-        <div className="text-center zoom">
-		    <LibroTapa imgPortada={libro.tapa}/>
-
-        </div>
-        
-        <div  className="card-body flex-fill pb-0" style={{ "width":"250px", "height":"320px"}} id="hov"> 
-          <h5 className="container text-center">{libro.titulo}</h5>
-
-          <div className="text-center">
-            <Badge pill variant="primary">
-              {" "}
-              {libro.categoria}{" "}
-            </Badge>{" "}
-            <Badge pill variant="info">
-              {" "}
-              {libro.clasificacion}{" "}
-            </Badge>{" "}
+    <section className="d-flex" id="cardslibros">
+      <div className=" mb-0">
+        <div className="card h-100  mt-5">
+          <div className="text-center zoom">
+            <LibroTapa imgPortada={libro.tapa} />
           </div>
 
-          <div className="text-left">
-            <p> {libro.descripcion} </p>
-          </div>
-          <div className="col px-0 ml-0  text-center">
-            <span className="h2 text-center">$ {libro.precio} </span>
-            <br />
-            <div className="row">
-              <div className="col px-0 ml-0 text-center">
-                <Button  variant="primary">Comprar</Button>
+          <div
+            className="card-body flex-fill pb-0"
+            style={{ width: "250px", height: "320px" }}
+          >
+            <h5 className="container text-center">{libro.titulo}</h5>
+
+            <div className="text-center">
+              <Badge pill variant="primary">
+                {" "}
+                {libro.categoria}{" "}
+              </Badge>{" "}
+              <Badge pill variant="info">
+                {" "}
+                {libro.clasificacion}{" "}
+              </Badge>{" "}
+            </div>
+
+            <div className="text-left">
+              <p> {libro.descripcion} </p>
+            </div>
+            <div className="col px-0 ml-0  text-center">
+              <span className="h2 text-center">$ {libro.precio} </span>
+              <br />
+              <div className="row">
+                <div className="col px-0 ml-0 text-center">
+                  <Button variant="primary">Comprar</Button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        </div>
       </div>
-
     </section>
   );
 }
--- src/componentes/LibroCardDeck.jsx
+++ src/componentes/LibroCardDeck.jsx
@@ -1,30 +1,22 @@
 import React from "react";
 import LibroCard from "./LibroCard";
-import { Container, Row, Col, CardDeck } from "react-bootstrap";
 
 
 export default function LibroCardDeck({ libros }) {
   return (
-    
     <>
-    <div className="row" style={{justifyContent:"center"}}>
+      <div className="row" style={{ justifyContent: "center" }}>
         {libros.map((item) => {
           return (
-            
-           
-            <div 
+            <div
               className="card-deck  justify-content-center col-12 col-sm-6 col-md-4 col-lg-3 mb-5 mt-4 mr-1 pr-1 d-flex"
               key={item.id}
             >
               <LibroCard libro={item} />
             </div>
-          
-           
           );
         })}
-         </div>
-      </>
-  
-  
+      </div>
+    </>
   );
 }
