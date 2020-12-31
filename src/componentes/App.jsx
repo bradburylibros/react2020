59470e185983c363dad068ea99bf096d2dbc40ac
Franco <francoricci_93@hotmail.com>
2020-12-31 17:09:28 -0300

    Agregado componente NotFound

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import AdminLibros from '../pages/AdminLibros'
 import Home from '../pages/Home'
 import Layout from './Layout'
+import NotFound from './NotFound';
 
 
 export default function App() {
@@ -13,6 +14,8 @@ export default function App() {
 					<Route exact path='/' component={Home} />
 					<Route exact path='/adminLibros' component={AdminLibros} />
 					{/* <Route path='/adminUsuarios' component={AdminUsuarios} /> */}
+
+					<Route component ={NotFound} />
 				</Switch>
 			</Layout>
 		</BrowserRouter>
