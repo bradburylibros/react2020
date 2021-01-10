dee3b8ae468423b5810a2d082121457b0838b758
Silvia Sosa <silvia_sosa@hotmail.com>
2021-01-09 21:45:53 -0300

    arreglando los links del footer, check el link Ingresar

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -5,9 +5,10 @@ import Layout from './Layout'
 import Home from '../pages/Home'
 import NotFound from './NotFound';
 import AdminDatos from '../pages/AdminDatos';
+import Login from '../componentes/LoginForm';
 
 window.globalVar = "http://localhost:3005";
-// window.globalVar = "https://cors-anywhere.herokuapp.com/https://mighty-peak-31803.herokuapp.com";
+// window.globalVar = "https://cors-anywhere.herokuapp.com";
 // window.globalVar = "https://mighty-peak-31803.herokuapp.com";
 // window.globalVar = "https://whispering-journey-97600.herokuapp.com"
 
@@ -17,6 +18,7 @@ export default function App() {
 			<Layout>
 				<Switch>
 					<Route exact path='/' component={Home} />
+					<Route exact path='/login' component={Login} />
 					<Route exact path='/adminLibros' component={AdminDatos} />
 					<Route component ={NotFound} />
 				</Switch>
